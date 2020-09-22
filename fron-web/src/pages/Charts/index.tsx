d657fa1892b1f3b3d451480ab1c331f26de47857
Rhuan Pablo Cesário Silva <rhuanpablo13@hotmail.com>
2020-09-22 13:19:36 -0300

    alterando nome do site

--- fron-web/src/pages/Charts/index.tsx
+++ fron-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'https://sds1-rhuan.herokuapp.com';
+const BASE_URL = 'https://rhuandevsuperior.netlify.app';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
